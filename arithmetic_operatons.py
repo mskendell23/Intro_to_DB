2a721ac300834eb0be573aebecace236fa5f6479
Emmanuella Nana Yaa Adjei <eladyaa15@gmail.com>
2025-06-08 13:59:00 +0000

    File edited

--- arithmetic_operatons.py
+++ arithmetic_operatons.py
@@ -1,4 +1,17 @@
 def perform_operation(num1, num2, operation):
+    """
+    Performs basic arithmetic operations based on the provided numbers and operation.
+
+    Args:
+        num1 (float): The first number.
+        num2 (float): The second number.
+        operation (str): The arithmetic operation to perform ("add", "subtract", "multiply", "divide").
+
+    Returns:
+        float: The result of the operation.
+        str: An error message for division  by zero or invalid operation.
+    """
+    
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
